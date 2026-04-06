c19288cdf0ef15135e228e6b25dc86dd39bb26d7
Diego Ramirez <dialrasa2@gmail.com>
2026-04-06 05:59:30 -0500

    feat(poster): wire middleware stack, sessions, and extended HTTP API

--- poster/server.js
+++ poster/server.js
@@ -1,4 +1,9 @@
+const crypto = require("crypto");
 const Butter = require("../butter");
+const staticFiles = require("./middlewares/static-files");
+const userRoutes = require("./middlewares/user-routes");
+const { SESSIONS, authMiddleware } = require("./middlewares/auth");
+const dataMiddleware = require("./middlewares/data");
 
 const USERS = [
   { id: 1, name: "John Doe", username: "john.doe", password: "string" },
@@ -16,27 +21,17 @@ const POSTS = [
 ];
 
 const server = new Butter();
-const PORT = 8000;
+const PORT = 9000;
 const HOSTNAME = "localhost";
 
 server.listen(PORT, HOSTNAME);
 
-server.route("/", "GET", (req, res) => {
-  res.sendFile("./public/index.html", "text/html");
-});
+server.use(userRoutes);
+server.use(staticFiles("./public"));
+server.use(authMiddleware);
+server.use(dataMiddleware);
 
-server.route("/login", "GET", (req, res) => {
-  res.sendFile("./public/index.html", "text/html");
-});
-
-server.route("/styles.css", "GET", (req, res) => {
-  res.sendFile("./public/styles.css", "text/css");
-});
-
-server.route("/scripts.js", "GET", (req, res) => {
-  res.sendFile("./public/scripts.js", "text/javascript");
-});
-
+// Posts
 server.route("/api/posts", "GET", (req, res) => {
   res.writeHead(200, { "Content-Type": "application/json" });
   const posts = POSTS.map((post) => {
@@ -49,22 +44,67 @@ server.route("/api/posts", "GET", (req, res) => {
   res.end(JSON.stringify(posts));
 });
 
+server.route("/api/posts", "POST", (req, res) => {
+  const { title, body } = req.body;
+  const post = {
+    id: POSTS.length + 1,
+    title,
+    body,
+    userId: req.session.userId,
+  };
+  POSTS.push(post);
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(JSON.stringify({ message: "Post created successfully" }));
+});
+
+// Login
 server.route("/api/login", "POST", (req, res) => {
-  let data = "";
-  req.on("data", (chunk) => {
-    data = data + chunk.toString();
+  const { username, password } = req.body;
+  const user = USERS.find(
+    (user) => user.username === username && user.password === password,
+  );
+
+  if (!user) {
+    res.writeHead(401, { "Content-Type": "application/json" });
+    return res.end(JSON.stringify({ message: "Invalid username or password" }));
+  }
+
+  const session = {
+    id: crypto.randomUUID(),
+    userId: user.id,
+  };
+
+  SESSIONS.push(session);
+
+  res.writeHead(200, {
+    "Content-Type": "application/json",
+    "Set-Cookie": `sessionId=${session.id}; Path=/`,
   });
-  req.on("end", () => {
-    const { username, password } = JSON.parse(data);
-    const user = USERS.find((user) => user.username === username && user.password === password);
-    
-    if (!user) {
-      res.writeHead(401, { "Content-Type": "application/json" });
-      res.end(JSON.stringify({ message: "Invalid username or password" }));
-      return;
-    }
-    
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify({ message: "Login successful" }));
+  res.end(JSON.stringify({ message: "Login successful" }));
+});
+
+server.route("/api/logout", "DELETE", (req, res) => {
+  SESSIONS.splice(SESSIONS.indexOf(req.session), 1);
+  res.writeHead(200, {
+    "Content-Type": "application/json",
+    "Set-Cookie": "sessionId=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT",
   });
+  res.end(JSON.stringify({ message: "Logout successful" }));
+});
+
+// User
+server.route("/api/user", "GET", (req, res) => {
+  const user = USERS.find((user) => user.id === req.session.userId);
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(JSON.stringify({ username: user.username, name: user.name }));
+});
+
+server.route("/api/user", "PUT", (req, res) => {
+  const { name, password, username } = req.body;
+  const user = USERS.find((user) => user.id === req.session.userId);
+  user.name = name;
+  user.password = password;
+  user.username = username;
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(JSON.stringify({ message: "User updated successfully" }));
 });
