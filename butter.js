b8c058a345fba165396f2f4f546fa713c8a4dfcf
Diego Ramirez <dialrasa2@gmail.com>
2026-04-06 05:59:26 -0500

    feat(butter)!: add middleware pipeline and method-prefixed route keys

--- butter.js
+++ butter.js
@@ -4,38 +4,50 @@ const fs = require("node:fs/promises");
 class Butter {
   constructor() {
     this.server = http.createServer();
-    // { "/": { GET: callback, POST: callback } }
+    // { "GET/": callback, "POST/": callback }
     this.routes = {};
+    this.middleware = [];
 
     this.server.on("request", (req, res) => {
       const { url, method } = req;
-      const route = this.routes[url];
+      const route = this.routes[method + url];
 
       res.sendFile = async (path, contentType) => {
         res.writeHead(200, { "Content-Type": contentType });
         const fileHandle = await fs.open(path, "r");
         const fileStream = fileHandle.createReadStream();
         fileStream.pipe(res);
-        
+
         fileHandle.on("end", () => {
           fileHandle.close();
         });
-      }
+      };
 
-      if (!route) {
-        res.writeHead(404, { "Content-Type": "text/html" });
-        res.end("<h1>404 Not Found</h1>");
-        return;
-      }
+      let index = 0;
+      const next = () => {
+        if (index < this.middleware.length) {
+          const middleware = this.middleware[index++];
+          middleware(req, res, next);
+        } else {
+          if (route) {
+            route(req, res);
+          } else {
+            res.writeHead(404, { "Content-Type": "text/html" });
+            res.end("<h1>404 Not Found</h1>");
+          }
+        }
+      };
 
-      route[method](req, res);
-    })
+      next();
+    });
   }
 
   route(path, method, callback) {
-    this.routes[path] = {
-      [method]: callback
-    }
+    this.routes[method + path] = callback;
+  }
+
+  use(middleware) {
+    this.middleware.push(middleware);
   }
 
   listen(port, hostname) {
